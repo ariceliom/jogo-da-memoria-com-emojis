04395cb3ce768c9da2af3d5570db6bb00849999a
Alder Junio <33815682+Alderj@users.noreply.github.com>
2023-09-26 15:26:23 -0400

    criando estilo de girar e carta

---
+++ src/scripts/engine.js
@@ -0,0 +1,49 @@
+const emojis = [
+    "🚲",
+    "🚲",
+    "🥵",
+    "🥵",
+    "🤡",
+    "🤡",
+    "🐾",
+    "🐾",
+    "😎",
+    "😎",
+    "😻",
+    "😻",
+    "👽",
+    "👽",
+    "🦄",
+    "🦄",
+];
+let openCards = [];
+
+
+let shuffeEmojis = emojis.sort(()=>(Math.random() > 0.5 ? 2 : -1));
+
+for(let i=0; i < emojis.length; i++){
+
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffeEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box);
+
+}
+
+function handleClick(){
+    if(openCards.length < 2){
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if (openCards.length == 2){
+        setTimeout(checkMatch, 500);
+    }
+};
+
+
+function checkMatch(){
+    
+}
+
